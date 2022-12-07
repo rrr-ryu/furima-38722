a3fc1e87c8a33c624646daa7df47b2af39096dec
rrr-ryu <zero.requiem910219@gmail.com>
2022-12-07 16:24:05 +0900

    factories/users.rbの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname                  { Faker::Name.last_name }
     email                     { Faker::Internet.free_email }
-    password                  { Faker::Internet.password(min_length: 6) }
+    password                  { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation     { password }
     last_name                 { '富おカ' }
     first_name                { '義ゆウ' }
