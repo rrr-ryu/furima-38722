910e31179e7042cc07f97f83c2974e0293e3fa68
rrr-ryu <zero.requiem910219@gmail.com>
2022-12-07 21:32:48 +0900

    Itemsモデルアソシエーション追加、バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
 
   validates :price, presence: true, format: { with: /\A[0-9]+\z/ }, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   
-  with_options presence: true, numericality: { other_than: 1 } do
+  with_options numericality: { other_than: 1 , message: "can't be blank"} do
     validates :category_id
     validates :condition_id
     validates :delivery_charge_id
@@ -13,4 +13,12 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attched :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :sender
+  belongs_to :delivery_day
+  
 end
