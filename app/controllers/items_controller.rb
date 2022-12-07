d00ab4fd03195a81353cf313071bb1be100b93a6
rrr-ryu <zero.requiem910219@gmail.com>
2022-12-07 21:54:27 +0900

    itemsコントローラーnew,createアクション追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,27 @@
 class ItemsController < ApplicationController
   def index
+
+  end
+
+  def new
+    @item = Item.new
+  end
+  
+  def create
+    @item = Item.new(params[item_parameter])
+    if @item.save
+      redirect_to items_path
+    else
+      render :new
+    end
+  end
+  
+
+  private
+  def item_parameter
+    params
+    .require(:items)
+    .permit(:name, :cotent, :category_id, :condition_id, :delivery_charge_id, :sender_id, :delivery_day_id, :price, :image)
+    .merge(user_id: current_user.id)
   end
 end
