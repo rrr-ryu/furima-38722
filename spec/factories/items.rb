b14e315069b8d1e966907edc460628b15e358fb5
rrr-ryu <zero.requiem910219@gmail.com>
2022-12-08 11:28:09 +0900

    itemsfactory作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name                  {Faker::Lorem.word}
+    content               {Faker::Lorem.sentence}
+    category_id           {Faker::Number.between(from: 2, to: 11)}
+    condition_id          {Faker::Number.between(from: 2, to: 7)}
+    delivery_charge_id    {Faker::Number.between(from: 2, to: 3)}
+    sender_id             {Faker::Number.between(from: 2, to: 48)}
+    delivery_day_id       {Faker::Number.between(from: 2, to: 4)}
+    price                 {Faker::Number.between(from: 300, to: 9999999)}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('app/assets/images/item-sample.png'), filename: 'item-sample.png')
+    end
   end
 end
