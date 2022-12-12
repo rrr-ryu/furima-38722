685a73f56e50ff2f916671ef413c1aa5def79ef7
rrr-ryu <zero.requiem910219@gmail.com>
2022-12-12 14:11:06 +0900

    Ordersmodel作成

---
+++ db/migrate/20221212034306_create_orders.rb
@@ -0,0 +1,14 @@
+class CreateOrders < ActiveRecord::Migration[6.0]
+  def change
+    create_table :orders do |t|
+      t.string :pots_code,                null: false 
+      t.integer :sender_id,               null: false
+      t.string :town,                     null: false
+      t.string :house_number,             null: false
+      t.string :building_number
+      t.string :phone_number,             null: false
+      t.references :purchase,             null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
