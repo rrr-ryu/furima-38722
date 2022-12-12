68c9e0a4ad5c45dc76cf85490244508dd4c52d2d
rrr-ryu <zero.requiem910219@gmail.com>
2022-12-12 22:13:32 +0900

    クレジットカード購入機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,15 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
   def index
     @item = Item.find(params[:item_id])
+    @purchase_order = PurchaseOrder.new
   end
 
   def create
     @item = Item.find(params[:item_id])
     @purchase_order = PurchaseOrder.new(params_purchase)
-    if 
+    if @purchase_order.valid?
+      pay_item
       @purchase_order.save
       redirect_to root_path
     else
@@ -16,7 +19,16 @@ class OrdersController < ApplicationController
 
   private
   def params_purchase
-    params.permit(:post_code, :sender_id, :town, :house_number, :building_number, :phone_number)
-          .merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_order).permit(:post_code, :sender_id, :town, :house_number, :building_number, :phone_number)
+          .merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end  
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: params_purchase[:token],
+      currency: 'jpy'
+    )
+  end
 end
--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -1,14 +1,14 @@
 class PurchaseOrder
   include ActiveModel::Model
-  attr_accessor :post_code, :sender_id, :town, :house_number, :building_number, :phone_number, :purchase_id, :item_id, :user_id
+  attr_accessor :post_code, :sender_id, :town, :house_number, :building_number, :phone_number, :purchase_id, :item_id, :user_id, :token
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :town
     validates :house_number
     validates :phone_number, format: {with: /\A[0-9]{10,11}\z/}
-    validates :purchase_id
     validates :item_id
     validates :user_id
+    validates :token
   end
 
   validates :sender_id, numericality: { other_than: 1, message: "can't be blank" }
