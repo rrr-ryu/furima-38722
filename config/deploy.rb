a6e95add4a82707d24972a7af476b60bc0ffa098
rrr-ryu <zero.requiem910219@gmail.com>
2022-12-15 00:21:15 +0900

    setbranch追加

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,6 +17,7 @@ set :rbenv_ruby, '2.6.5' #カリキュラム通りに進めた場合、’2.6.5�
 set :ssh_options, auth_methods: ['publickey'],
                                   keys: ['~/.ssh/rrrRyu.pem'] 
 
+set :branch, ENV['BRANCH'] || "main"                                 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
