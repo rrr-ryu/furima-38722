e7fe69ef0633b931c377b1b5df933b9c1dbd7b0c
rrr-ryu <zero.requiem910219@gmail.com>
2023-01-09 10:58:18 +0900

    payjp v2移行修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,33 +1,29 @@
 function pay() {
-  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
-    const submit = document.getElementById("button");
-  submit.addEventListener('click',(e) => {
+  const payjp = Payjp(process.env.PAYJP_PUBLIC_KEY)
+  const elements = payjp.elements();
+
+  const numberElement = elements.create('cardNumber');
+  const expiryElement = elements.create('cardExpiry');
+  const cvcElement = elements.create('cardCvc');
+
+  numberElement.mount('#number-form');
+  expiryElement.mount('#expiry-form');
+  cvcElement.mount('#cvc-form');
+
+  const submit = document.getElementById("button");
+  submit.addEventListener("click", (e) => {
     e.preventDefault();
-    const formResult = document.getElementById('charge-form');
-    const formData = new FormData(formResult);
-
-    const card = {
-      number: formData.get('purchase_order[number]'),
-      exp_month: formData.get('purchase_order[exp_month]'),
-      exp_year: `20${formData.get("purchase_order[exp_year]")}`,
-      cvc: formData.get('purchase_order[cvc]')
-    };
-
-    Payjp.createToken(card, (status, response) => {
-      if(status == 200) {
+    payjp.createToken(numberElement).then(function (response) {
+      if (response.error) {
+      } else {
         const token = response.id;
-        const renderDom = document.getElementById('charge-form');
-        const tokenObj = `<input value=${token} name='token' type="hidden"> `;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} type="hidden" name='token'>`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
-      };
-
-      document.getElementById('card-number').removeAttribute('name');
-      document.getElementById('card-exp-month').removeAttribute('name');
-      document.getElementById('card-exp-year').removeAttribute('name');
-      document.getElementById('card-cvc').removeAttribute('name');
-
-      document.getElementById('charge-form').submit();
+      }
+      document.getElementById("charge-form").submit();
     });
-  });
+  })
 };
+
 window.addEventListener('load', pay);
